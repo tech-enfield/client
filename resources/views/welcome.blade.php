f666fde295b91a6e97593e04ed241f3159ec37e5
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-08-14 11:07:18 +0000

    welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
                             It Works</a>
                         <a href="#commission"
                             class="text-gray-700 hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium transition duration-300">Commission</a>
-                        <a href="{{ route('register') }}"
+                        <a href="{{ route('login') }}"
                             class="bg-blue-600 text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-blue-700 transition duration-300">Get
                             Started</a>
                     </div>
